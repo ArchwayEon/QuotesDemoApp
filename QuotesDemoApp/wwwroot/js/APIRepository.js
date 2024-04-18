e3d9d02bfbe7149ef36b91eb95029694a70c3c68
Roach, Jeff <ROACHJ@ETSU.EDU>
2024-04-18 14:16:59 -0400

    Add project files.

---
+++ QuotesDemoApp/wwwroot/js/APIRepository.js
@@ -0,0 +1,86 @@
+﻿"use strict";
+export class APIRepository {
+    #baseAddress;
+    #objectName;
+    // Constructs the repository
+    // baseAddress : string
+    //    The URL of the base address. e.g."https://somehost/api/somename"
+    // objectName : string 
+    //    The name of the object. e.g. pet
+    constructor(baseAddress, objectName) {
+        this.#baseAddress = baseAddress;
+        this.#objectName = objectName;
+    }
+
+    // Retrieves all the objects (GET)
+    // Returns: JSON
+    // Throws: Error - if there was an HTTP error.
+    async readAll() {
+        const address = `${this.#baseAddress}/all`;
+        const response = await fetch(address);
+        if (!response.ok) {
+            throw new Error(`There was an HTTP error getting the ${this.#objectName} data.`);
+        }
+        return await response.json();
+    }
+
+    // Retrievas one object (GET)
+    // Returns: JSON
+    // Throws: Error - if there was an HTTP error.
+    async read(id) {
+        const address = `${this.#baseAddress}/one/${id}`;
+        const response = await fetch(address);
+        if (!response.ok) {
+            throw new Error(`There was an HTTP error getting the ${this.#objectName} data.`);
+        }
+        return await response.json();
+    }
+
+    // Creates a new object (POST)
+    // formData : FormData
+    // Returns: JSON
+    // Throws: Error - if there was an HTTP error.
+    async create(formData) {
+        const address = `${this.#baseAddress}/create`;
+        const response = await fetch(address, {
+            method: "post",
+            body: formData
+        });
+        if (!response.ok) {
+            throw new Error(`There was an HTTP error creating the ${this.#objectName} data.`);
+        }
+        return await response.json();
+    }
+
+    // Updates an existing object (PUT)
+    // formData : FormData
+    // Returns: text
+    // Throws: Error - if there was an HTTP error.
+    async update(formData) {
+        const address = `${this.#baseAddress}/update`;
+        const response = await fetch(address, {
+            method: "put",
+            body: formData
+        });
+        if (!response.ok) {
+            throw new Error(`There was an HTTP error updating the ${this.#objectName} data.`);
+        }
+        return await response.text();
+    }
+
+    // Deletes an object (PUT)
+    // id : string - The id if the object
+    // Returns: text
+    // Throws: Error - if there was an HTTP error.
+    async deletePet(id) {
+        const address = `${this.#baseAddress}/delete/${id}`;
+        const response = await fetch(address, {
+            method: "delete"
+        });
+        if (!response.ok) {
+            throw new Error(`There was an HTTP error deleting the ${this.#objectName} data.`);
+        }
+        return await response.text();
+    }
+
+}
---
+++ QuotesDemoApp/wwwroot/js/DOM.js
@@ -0,0 +1,81 @@
+﻿"use strict";
+
+export class DOM {
+    // Creates a <TD> DOM element and inserts text node into it.
+    // text : string
+    // Returns: Element
+    createTextTD(text) {
+        const td = document.createElement("td");
+        td.appendChild(document.createTextNode(text));
+        return td;
+    }
+
+    // Creates a <TR> DOM element and inserts a <TD> element into it.
+    // src : string - URL of the image
+    // alt : string - The alternative text for the image
+    // Returns: Element
+    createImageTR(src, alt) {
+        const tr = document.createElement("tr");
+        const td = this.createImageTD(src, alt);
+        td.setAttribute("colspan", "4");
+        tr.appendChild(td);
+        return tr;
+    }
+
+    // Creates a <TD> DOM element and inserts an <IMG> element into it.
+    // src : string - URL of the image
+    // alt : string - The alternative text for the image
+    // Returns: Element
+    createImageTD(src, alt) {
+        const td = document.createElement("td");
+        const img = this.createImg(src, alt);
+        td.appendChild(img);
+        return td;
+    }
+
+    // Creates an <IMG> DOM element.
+    // src : string - URL of the image
+    // alt : string - The alternative text for the image
+    // Returns: Element
+    createImg(src, alt) {
+        const img = document.createElement("img");
+        img.setAttribute("src", src);
+        img.setAttribute("alt", alt);
+        return img;
+    }
+
+    // Creates an <A> DOM element with a text node.
+    // url : string - URL of the link
+    // text : string
+    // Returns: Element
+    createTextLink(url, text) {
+        const a = document.createElement("a");
+        a.setAttribute("href", url);
+        a.appendChild(document.createTextNode(text));
+        return a;
+    }
+
+    // Removes all the children of element
+    // element : Element
+    removeChildren(element) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    }
+
+    // Creates a text node and appends it to the element with id elementId
+    // elementId : string - CSS id selector 
+    // text : string
+    setElementText(elementId, text) {
+        const element = document.querySelector(elementId);
+        element.appendChild(document.createTextNode(text));
+    }
+
+    // Sets the value of the element with id elementId
+    // elementId : string - CSS id selector
+    // value : any
+    setElementValue(elementId, value) {
+        const element = document.querySelector(elementId);
+        element.value = value;
+    }
+}
